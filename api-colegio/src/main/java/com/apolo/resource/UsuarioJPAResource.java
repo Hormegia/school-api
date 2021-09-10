28ba65cd2c5c67602f58b8f28597c5df6760988c
nmartinezmarin <nicomako96@hotmail.com>
2021-09-09 22:00:53 -0500

    refactorización del proyecto

---
+++ api-colegio/src/main/java/com/apolo/resource/UsuarioJPAResource.java
@@ -0,0 +1,157 @@
+package com.apolo.resource;
+
+
+//import com.apolo.repository.UsuarioDaoService;
+
+import com.apolo.spring.exception.ObjetoNoEncontradoException;
+import com.apolo.model.Matricula;
+import com.apolo.model.TokenActivacionUsuario;
+import com.apolo.model.Usuario;
+import com.apolo.event.onRegistroUsuarioEvent;
+import com.apolo.repository.MatriculaRepository;
+import com.apolo.repository.UserRepository;
+import com.apolo.service.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+@RestController
+public class UsuarioJPAResource {
+
+
+    private final IUsuarioService iUsuarioService;
+
+    private final UserRepository userRepository;
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    private final MatriculaRepository matriculaRepository;
+
+    @Autowired
+    public UsuarioJPAResource(IUsuarioService iUsuarioService, UserRepository userRepository, MatriculaRepository matriculaRepository, ApplicationEventPublisher eventPublisher) {
+        this.iUsuarioService = iUsuarioService;
+        this.userRepository = userRepository;
+        this.matriculaRepository = matriculaRepository;
+        this.eventPublisher = eventPublisher;
+    }
+
+
+    //todos los usuarios
+    // GET  /usuario/
+    @GetMapping("/usuarios")
+    public List<Usuario> retrieveAllUsuarios(){
+        return userRepository.findAll();
+    }
+
+    //Trae un usuario por id
+    // GET  /usuarios/id
+    @GetMapping("/usuarios/{id}")
+    public EntityModel<Usuario> retrieveUsuario(@PathVariable int id){
+        Optional<Usuario> ususario = userRepository.findById(id);
+        if(!ususario.isPresent())
+            throw new ObjetoNoEncontradoException("id-"+id);
+
+
+        //HATEOAS
+        EntityModel<Usuario> resource = EntityModel.of(ususario.get());
+
+        WebMvcLinkBuilder linkTo =
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsuarios());
+
+        resource.add(linkTo.withRel("todos-us"));
+
+
+        return resource;
+    }
+
+
+    //eliminar usuario
+    //usuarios/id
+    @DeleteMapping("/usuarios/{id}")
+    public void deleteUsuario(@PathVariable int id){
+        userRepository.deleteById(id);
+    }
+
+    //Actualizar Usuario
+    //Se maneja otro para crear usuarios porque es necesario la autenticación
+    // Post  /usuarios/
+    @PostMapping("/usuarios")
+    public ResponseEntity<Object> createUsuario(@Valid @RequestBody Usuario usuario){
+
+        Usuario usuarioNevo = iUsuarioService.registrarUsuario(usuario);
+
+        String appUrl = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/activar").toUriString();
+        Locale locale = LocaleContextHolder.getLocale();
+
+        eventPublisher.publishEvent(new onRegistroUsuarioEvent(usuarioNevo,
+                appUrl, locale));
+
+        /* Responde en el header el lugar donde se puede encontrar la información
+         * del usuario creado
+         */
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(
+                usuarioNevo.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/usuarios/{id}/matriculas")
+    public List<Matricula> encontrasMatriculas(@PathVariable int id){
+        Optional<Usuario> userOptional = userRepository.findById(id);
+        if(!userOptional.isPresent()){
+            throw new ObjetoNoEncontradoException("id-" + id);
+        }
+        return userOptional.get().getMatriculas();
+    }
+
+    @GetMapping("/usuarios/activar/{token}")
+    public String activarUsuario(@PathVariable String token){
+        TokenActivacionUsuario tokenActivacionUsuario = iUsuarioService.getTokenActivacion(token);
+        if(tokenActivacionUsuario == null)
+            throw new ObjetoNoEncontradoException("Token Expirado");
+
+        Usuario usuario = iUsuarioService.getUsuario(token);
+
+        usuario.setHabilitado(true);
+        userRepository.save(usuario);
+
+        return "usuario habilitado";
+    }
+
+    @PostMapping("/usuarios/{id}/matriculas")
+    public ResponseEntity<Object> crearMatricula(@PathVariable int id, @RequestBody Matricula matricula){
+        Optional<Usuario> usuarioOptional = userRepository.findById(id);
+        if(!usuarioOptional.isPresent()){
+            throw new ObjetoNoEncontradoException("id-" + id);
+        }
+        Usuario usuario = usuarioOptional.get();
+
+        matricula.setUsuario(usuario);
+        matriculaRepository.save(matricula);
+
+        // /usuario/{id}
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(
+                        matricula.getId()).toUri();
+        System.out.println(ResponseEntity.created(location).build());
+
+        return ResponseEntity.created(location).build();
+    }
+}
---
+++ api-colegio/src/main/java/com/apolo/spring/exception/CustomizeResponseEntityExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.apolo.spring.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.List;
+
+@ControllerAdvice
+@RestController
+public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ObjetoNoEncontradoException.class)
+    public final ResponseEntity<Object> handleObjetoNoEncontrado(ObjetoNoEncontradoException ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ObjetoExistenteException.class)
+    public final ResponseEntity<Object> handleObjetoExistenteException(ObjetoExistenteException ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler({org.springframework.security.core.AuthenticationException.class})
+    public final ResponseEntity<Object> handleAuthenticationException(org.springframework.security.core.AuthenticationException ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        StringBuilder res = new StringBuilder();
+        for (FieldError fieldError: fieldErrors) {
+            res.append(String.format("campo: %s , regla: %s\n",fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation failed", res.toString());
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
