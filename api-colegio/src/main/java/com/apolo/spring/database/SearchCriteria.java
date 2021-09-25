a23fe2be2da5caf9eff593a71a7fba53d664922f
nmartinezmarin <nicomako96@hotmail.com>
2021-09-24 23:43:38 -0500

    se toma implementación de https://medium.com/backend-habit/spring-jpa-make-dynamic-where-using-predicate-and-criteria-84550dfaa182 para generar consultas con filtros de forma dinámica

---
+++ api-colegio/src/main/java/com/apolo/spring/database/SearchCriteria.java
@@ -0,0 +1,43 @@
+package com.apolo.spring.database;
+
+/**
+ * Esta clase se utiliza para crear criterios  de búsqueda  personalizados
+ * La implementación se toma de https://medium.com/backend-habit/spring-jpa-make-dynamic-where-using-predicate-and-criteria-84550dfaa182
+ */
+public class SearchCriteria {
+
+    private String key;
+    private Object value;
+    private SearchOperation operation;
+
+
+    public SearchCriteria(String key, Object value, SearchOperation operation) {
+        this.key = key;
+        this.value = value;
+        this.operation = operation;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public SearchOperation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(SearchOperation operation) {
+        this.operation = operation;
+    }
+}
