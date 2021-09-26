865cb1d1f5a1c74d7091dd9384d8d2d0afd0dfc5
nmartinezmarin <nicomako96@hotmail.com>
2021-09-25 19:07:25 -0500

    se agrega JpaSpecificationExecutor para personalizar las querys

--- api-colegio/src/main/java/com/apolo/repository/AcudienteRepository.java
+++ api-colegio/src/main/java/com/apolo/repository/AcudienteRepository.java
@@ -1,9 +1,12 @@
 package com.apolo.repository;
 
 import com.apolo.model.Acudiente;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AcudienteRepository extends JpaRepository<Acudiente, Integer> {
+public interface AcudienteRepository extends JpaRepository<Acudiente, Integer>, JpaSpecificationExecutor<Acudiente> {
+    
 }
